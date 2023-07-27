7b2df1c1ce906e60f13d937be75d79134216b3ec
azhardumac <azhardumac@gmail.com>
2023-07-27 20:06:04 +0530

    footerlogoupdate

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -13,8 +13,8 @@
                         </div>
                         <div class="footer-widget-content">
                             <div class="content">
-                                <a href="tel:(+1)212-946-2701">{{ footerData.footerAboutWidget.telephone }}</a>
-                                <a href="mailto:hello@hasagency.com">{{ footerData.footerAboutWidget.email }}</a>
+                                <a href="tel:08956786421">{{ footerData.footerAboutWidget.telephone }}</a>
+                                <a href="mailto:hello@whizfortune.com">{{ footerData.footerAboutWidget.email }}</a>
                             </div>
                             <div class="footer-social-inline">
                                 <a :href="footerData.footerAboutWidget.twitterUrl" target="_blank">
--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -8,8 +8,8 @@
                     <div class="col-xl-2 col-auto order-0">
                         <div class="header-logo">
                             <NuxtLink to="/">
-                                <img class="dark-logo" src="/images/logo/whizlogo2.png" alt="Agency Logo">
-                                <img class="light-logo" src="/images/logo/logo.png" alt="Agency Logo">
+                                <img class="dark-logo" src="/images/logo/whizlogo2.png" alt="Whizfortune Logo">
+                                <img class="light-logo" src="/images/logo/whizlogo2.png" alt="Whizfortune Logo">
                             </NuxtLink>
                         </div>
                     </div>
